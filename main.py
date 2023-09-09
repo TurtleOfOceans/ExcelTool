be9cab9e9794940f3fa3892fc4e6ee24b46f6a54
TurtleOfOceans <oanhkid@gmail.com>
2023-09-09 18:29:03 +0700

    Update filter color

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # create by TurtleOfOceans 06/09/2023
 import os
+import openpyxl
 import pandas as pd
 from tqdm import tqdm
 
 
-def readExcel(input_dir, sheet=0):
+def readExcelWithPandas(input_dir, sheet=0):
     """
-    read excel file
+    read excel file with pandas lib
     Args:
         input_dir: excel director
         sheet: specify sheet to read data
@@ -16,6 +17,20 @@ def readExcel(input_dir, sheet=0):
     excel_data = pd.read_excel(input_dir, sheet)
     excel_data.columns.values
     return excel_data
+
+
+def readExcelWithOpenpyxl(input_dir, sheet=0):
+    """
+    read excel file with openpyxl lib
+    Args:
+        input_dir: excel director
+        sheet: specify sheet to read data
+    return:
+        work_sheet: sheet data
+    """
+    work_book = openpyxl.load_workbook(input_dir, data_only=True)
+    work_sheet = work_book.worksheets[sheet]
+    return work_sheet
 
 
 def getListHeader(excel_data):
@@ -148,6 +163,25 @@ def inputSplitParam():
     return param
 
 
+def inputParamForCreateFilter():
+    """
+    ask the user to enter input
+    return:
+        param: contain data specify by user
+    """
+    print("==========================")
+    input_dir = input("Input (excel file): ")
+    output_dir = input("Output (specify folder): ")
+    active_sheet = input("Specify sheet active [0, 1, ...]: ")
+    print("==========================")
+    param = {
+        'inputDir': input_dir,
+        'outputDir': output_dir,
+        'activeSheet': int(active_sheet),
+    }
+    return param
+
+
 def splitDataWithMode(input_dir, output_dir, filter, sheet, mode):
     """
     read file excel and split
@@ -158,7 +192,7 @@ def splitDataWithMode(input_dir, output_dir, filter, sheet, mode):
         sheet: specify sheet by user
         mode: specify mode by user
     """
-    excel_data = readExcel(input_dir, sheet)
+    excel_data = readExcelWithPandas(input_dir, sheet)
     header = getListHeader(excel_data)
     filter_data = getListDataColumn(excel_data, header[filter])
     if (mode == 0):
@@ -191,11 +225,107 @@ def splitToMultiDataWithMode(mode):
     print("===========DONE===========")
 
 
+def createFileWithOpenpyxl(rows_data, output_dir):
+    """
+    create file with openxlsx
+    Args:
+        output_dir: specify out put folder
+        data: out put data
+    """
+    total = len(rows_data)
+    print("Total: ", total)
+    progresBar = tqdm(range(total), desc="Create Fille...")
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    for i, row_data in enumerate(rows_data):
+        for j in range(0, 13):
+            sheet.cell(i + 1, j + 1).value = row_data[j].value
+            progresBar.update()
+
+    file_name = os.path.join(output_dir, f'Test.xlsx')
+    wb.save(file_name)
+
+
+def isRowNeed(row, min_clolumns, max_columns, row_number):
+    """
+    check row is copy
+    Args:
+        input_dir: specify input file
+        sheet: active sheet
+    """
+    BG_COLOR = 64
+    FG_COLOR = "FFFFFF00"
+    isRow = True
+    for i in range(min_clolumns, max_columns):
+        cell = row[i]
+        value = cell.value
+        bg_color = cell.fill.bgColor.index
+        fg_color = cell.fill.fgColor.index
+        if value == None:
+            continue
+        if bg_color == BG_COLOR and fg_color == FG_COLOR:
+            continue
+        else:
+            isRow = False
+            break
+
+    if isRow:
+        for i in range(min_clolumns, max_columns):
+            cell = row[i]
+            if cell.value != None:
+                bg_color = cell.fill.bgColor.index
+                fg_color = cell.fill.fgColor.index
+                if bg_color != BG_COLOR or fg_color != FG_COLOR:
+                    isRow = False
+                    break
+    return isRow
+
+
+def createTableDataWithColorFilter(input_dir, sheet, output_dir):
+    """
+    read and create filter color
+    Args:
+        input_dir: specify input file
+        sheet: active sheet
+        output_dir: specify output dir
+    """
+    rows_need = []
+    work_sheet = readExcelWithOpenpyxl(input_dir, sheet)
+    total = 0
+    for i in range(2, work_sheet.max_row):
+        row = work_sheet[i]
+        isRow = isRowNeed(row, 0, 13, i)
+        if isRow == True:
+            # print(f'row[{i}] = {isRow}')
+            total += 1
+            rows_need.append(row)
+    rows_length = len(rows_need)
+    print(f'rows_length = {rows_length}, total = {total}')
+    createFileWithOpenpyxl(rows_need, output_dir)
+
+
+def createFilterColorMode(mode):
+    """
+    read and create filter color
+    Args:
+        mode: selected mode
+    """
+    param = inputParamForCreateFilter()
+    input = param.get('inputDir')
+    output = param.get('outputDir')
+    sheet = param.get('activeSheet', 0)
+    createTableDataWithColorFilter(input, sheet, output)
+    print("===========DONE===========")
+
+
 def selectMode():
     """
     ask the user to specify mode
     """
-    modes = ["Split to multiple file", "Split to multiple sheet"]
+    modes = [
+        "Split to multiple file",
+        "Split to multiple sheet",
+        "check color Mode"]
     print("All mode:")
     i = 0
     for mode in modes:
@@ -216,6 +346,8 @@ def run(mode):
         splitToMultiDataWithMode(mode)
     elif (mode == 1):
         splitToMultiDataWithMode(mode)
+    elif (mode == 2):
+        createFilterColorMode(mode)
     else:
         print("mode dont exis")
 
